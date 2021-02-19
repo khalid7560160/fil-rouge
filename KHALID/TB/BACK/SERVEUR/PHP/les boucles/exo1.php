aeb3b9145fa4e9dd29b5e0011be8d39195648855
80010-78-06 <80010-78-06@exchange.ad.afpanet>
2021-02-19 10:50:49 +0100

    new

---
+++ KHALID/TB/BACK/SERVEUR/PHP/les boucles/exo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+
+<?php
+//Ecrire un script PHP qui affiche tous les nombres impairs entre 0 et 150, par ordre croissant : 1 3 5 7...
+
+
+// déterminer si le nombre est pair ou impair
+
+for ($nombre = 1; $nombre <= 150; $nombre++) {
+    echo "$nombre";
+    echo "<br />";
+    $nombre++;
+}
+?> 
+
+</body>
+</html>
---
+++ KHALID/TB/BACK/SERVEUR/PHP/les boucles/exo2.php
@@ -0,0 +1,7 @@
+//Écrire un programme qui écrit 500 fois la phrase Je dois faire des sauvegardes régulières de mes fichiers.
+
+$x = 1;
+do {
+    echo "$x - Je dois faire des sauvegardes régulières de mes fichiers.<br>";
+    $x++;
+} while ($x <= 500);
